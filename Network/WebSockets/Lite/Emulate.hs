cfdb8ecd1f98608c71711cc66781bacbe47d4248
yihuang <yi.codeplayer@gmail.com>
2011-12-16 16:38:10 +0800

    remove StreamChan stuff, don't belong here.

--- Network/WebSockets/Lite/Emulate.hs
+++ Network/WebSockets/Lite/Emulate.hs
@@ -1,8 +1,5 @@
 module Network.WebSockets.Lite.Emulate
-  ( StreamChan
-  , iterStreamChan
-  , enumStreamChan
-  , runWSLite
+  ( runWSLite
   ) where
 
 import Data.ByteString (ByteString)
@@ -10,21 +7,7 @@ import Prelude hiding (catch)
 import Control.Exception
 import Control.Monad.IO.Class
 import Data.Enumerator
-import Control.Concurrent.STM
 import Network.WebSockets.Lite
-
-type StreamChan a = TChan (Stream a)
-
-iterStreamChan :: StreamChan a -> Iteratee a IO ()
-iterStreamChan ch = continue go
-  where
-    go EOF = liftIO $ atomically $ writeTChan ch EOF
-    go stream = liftIO (atomically $ writeTChan ch stream) >> continue go
-
-enumStreamChan :: StreamChan a -> Enumerator a IO b
-enumStreamChan ch = checkContinue0 $ \loop f -> do
-    stream <- liftIO $ atomically $ readTChan ch
-    f stream >>== loop
 
 runWSLite :: Enumerator ByteString IO () -> (ByteString -> IO ()) -> WSLite () -> IO ()
 runWSLite enum sink lite = do
